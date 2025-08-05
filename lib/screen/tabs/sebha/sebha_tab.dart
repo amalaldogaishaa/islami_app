3e9856ccd1d69c90abbed855bc0662273c6cbf0d
amalaldogaish <amalaldogaishaa@gmail.com>
2025-08-05 21:05:07 +0300

    Update sebha screen

--- lib/screen/tabs/sebha/sebha_tab.dart
+++ lib/screen/tabs/sebha/sebha_tab.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SebhaTab extends StatefulWidget {
@@ -10,6 +12,7 @@ class SebhaTab extends StatefulWidget {
 class _SebhaTabState extends State<SebhaTab> {
   int counter = 0;
   int highlightedBead = 0;
+  double angle = 0;
   final List<String> tasbeehList = ['سبحان الله', 'الحمد لله', 'الله أكبر'];
 
   String get currentTasbeeh {
@@ -19,6 +22,7 @@ class _SebhaTabState extends State<SebhaTab> {
 
   void incrementCounter() {
     setState(() {
+      angle += 15 * pi / 180;
       counter++;
       highlightedBead = (highlightedBead + 1) % 33;
     });
@@ -26,63 +30,69 @@ class _SebhaTabState extends State<SebhaTab> {
 
   @override
   Widget build(BuildContext context) {
-    double sebhaSize = MediaQuery.sizeOf(context).width * 0.9;
+    var ScreenSize = MediaQuery.sizeOf(context);
 
-    return Column(
-      children: [
-        const SizedBox(height: 8),
-        Text(
-          'سَبِّحِ اسْمَ رَبِّكَ الْأَعْلَى',
-          style: Theme.of(context).textTheme.titleLarge!.copyWith(
-            fontSize: 36,
-            fontWeight: FontWeight.w700,
-            fontFamily: 'Janna',
-          ),
-        ),
-        const SizedBox(height: 10),
-        Positioned(
-          child: Image.asset('assets/images/SebhaHeader.png', width: 100),
-        ),
-        Center(
-          child: GestureDetector(
-            onTap: incrementCounter,
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/SebhaBody.png',
-                  width: sebhaSize,
-                  height: sebhaSize,
-                  fit: BoxFit.fill,
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const SizedBox(height: 20),
-                    Text(
-                      currentTasbeeh,
-                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                        fontSize: 36,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: 'Janna',
-                      ),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      '$counter',
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          const SizedBox(height: 8),
+          Text(
+            'سَبِّحِ اسْمَ رَبِّكَ الْأَعْلَى',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              fontSize: 36,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Janna',
             ),
           ),
-        ),
-      ],
+          SizedBox(height: ScreenSize.height * 0.02),
+          Stack(
+            alignment: Alignment.topCenter,
+            children: [
+              Image.asset('assets/images/SebhaHeader.png'),
+              Padding(
+                padding: EdgeInsets.only(top: ScreenSize.width * 0.18),
+                child: SizedBox(
+                  width: ScreenSize.width * 0.85,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      InkWell(
+                        onTap: incrementCounter,
+                        child: Transform.rotate(
+                          angle: angle,
+                          child: Image.asset('assets/images/SebhaBody.png'),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            currentTasbeeh,
+                            style: Theme.of(context).textTheme.titleLarge!
+                                .copyWith(
+                                  fontSize: 36,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: 'Janna',
+                                ),
+                          ),
+                          Text(
+                            '$counter',
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
